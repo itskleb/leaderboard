33829eb0f8113bf7d7ea6dadb8e3a43597561753
itskleb <itskleb@gmail.com>
2026-03-25 14:38:41 -0400

    Create leaderboard.py

---
+++ leaderboard.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+from datetime import datetime as dt
+
+st.set_page_config(page_title="Leaderboard", page_icon="🏆", layout="centered")
+
+st.title("🏆 Leaderboard")
+
+uploaded_file = st.file_uploader("Upload Memebrship XLSX file", type=["xlsx"])
+uploaded_file_ny = st.file_uploader("Upload New Youth XLSX file", type=["xlsx"])
+
+months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+mon_dict = dict(zip([i for i in range(1,13)],months))
+
+try:
+    df = pd.read_csv('Monthly Membership by unit.csv')
+    df = df.drop('Unnamed: 0')
+except:
+    df = pd.DataFrame(columns=['Unique','Boro','District','Unit','Order',
+                               'January','February','March','April','May','June',
+                               'July','August','September','October','November','December'])
+try:
+    df_net = pd.read_csv('Net change by month.csv')
+    df_net = df_net.set_index('Unique').drop('Unnamed: 0',axis=1)
+except:
+    df_net = pd.DataFrame(columns=['Unique','Boro','District','Unit','Order',
+                                'January','February','March','April','May','June',
+                                'July','August','September','October','November','December'])
+try:
+    df_ny = pd.DataFrame(columns=['Unique','Boro','District','Unit','Order',
+                                'January','February','March','April','May','June',
+                                'July','August','September','October','November','December'])
+except:
+    pass
+
+
+
+full = pd.read_excel(uploaded_file,skiprows=2)
+month = pd.read_excel(uploaded_file).columns[0].split('\n')[2].split(' ')[3]
+curr_mon = dt.today().month
+
+if month == 'Current':
+    month = mon_dict[curr_mon]
+  
+re_name = {"CouncilNumber Hierarchy - District":'District',	
+           'CouncilNumber Hierarchy - SubDistrictName':'Boro', 
+           'Current Month':month,
+           'CouncilNumber Hierarchy - Unit':'Unit'}
+
+full = full.rename(re_name,axis=1)
+full = full[['Boro','District','Unit','Order',month]]
+full= full[~full['Boro'].isna()]
+full['Boro'] = full['Boro'].apply(lambda x: x.split(' (')[0].split(' 6')[0])
+full['Unique'] = full['Boro']+full['District']+full['Unit']
+
+if len(df) == 0:
+    df = pd.concat([df,full],axis = 0)
+    df_net = pd.concat([df_net,df[['Unique','Boro','District','Unit','Order']]],axis = 0)
+    df_net = df_net.set_index('Unique')
+else:
+    df[month] = df['Unique'].map(full.set_index('Unique')[month])
+    df = df.fillna(0.0)
+
+newbies = pd.read_excel(uploaded_file_ny,skiprows=2)
+newbies = newbies.rename(re_name,axis=1)
+newbies = newbies[['Boro','District','Unit','RegStatusxMonth','Month Year']]
+newbies= newbies[~newbies['Boro'].isna()]
+newbies['Boro'] = newbies['Boro'].apply(lambda x: x.split(' (')[0].split(' 6')[0])
+newbies['Unique'] = newbies['Boro']+newbies['District']+newbies['Unit']
+
+df_ny['Unique'] = df['Unique']
+df_ny['Boro'] = df['Boro']
+df_ny['District'] = df['District']
+df_ny['Order'] = df['Order']
+df_ny['Unit']= df['Unit']
+df_ny = df_ny.set_index('Unique').fillna(0.0)
+
+for col in months:
+    frame = newbies[newbies['Month Year']==col]
+    for _,row in frame.iterrows():
+        df_ny.loc[row.Unique,col] = row['RegStatusxMonth']
+        
+
+#curr_mon=2
+for _,row in df.iterrows():
+    curr = mon_dict[curr_mon]
+    if curr_mon != 1:
+        past = mon_dict[curr_mon-1]
+    else:
+        past = curr
+    net = row[curr] - row[past]
+    r=row.Unique
+    c=curr
+    df_net.loc[r,c] = net
+
+df_ny['Total New Youth'] = df_ny[months].sum(axis=1)
+
+df_ny['Net Change from January'] = df_net[months].sum(axis=1)
+df_ny['Current Size'] = df.set_index('Unique')[month]
+
+display = df_ny[['District', 'Unit','Order','Total New Youth','Net Change from January','Current Size']].reset_index().drop('Unique',axis=1)
+
+df.to_csv('Monthly Membership by unit.csv')
+df_net.to_csv('Net change by month.csv')
+df_ny.to_csv('New Youth.csv')
