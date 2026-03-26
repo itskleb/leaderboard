41473f75677bff675d7023fcdd5fab2e01ed784b
itskleb <itskleb@gmail.com>
2026-03-26 10:37:38 -0400

    fixed uploaded file bug

--- leaderboard.py
+++ leaderboard.py
@@ -35,72 +35,72 @@ except:
     pass
 
 
+if uploaded_file != None:
+    full = pd.read_excel(uploaded_file,skiprows=2)
+    month = pd.read_excel(uploaded_file).columns[0].split('\n')[2].split(' ')[3]
+    curr_mon = dt.today().month
 
-full = pd.read_excel(uploaded_file,skiprows=2)
-month = pd.read_excel(uploaded_file).columns[0].split('\n')[2].split(' ')[3]
-curr_mon = dt.today().month
+    if month == 'Current':
+        month = mon_dict[curr_mon]
+    
+    re_name = {"CouncilNumber Hierarchy - District":'District',	
+            'CouncilNumber Hierarchy - SubDistrictName':'Boro', 
+            'Current Month':month,
+            'CouncilNumber Hierarchy - Unit':'Unit'}
 
-if month == 'Current':
-    month = mon_dict[curr_mon]
-  
-re_name = {"CouncilNumber Hierarchy - District":'District',	
-           'CouncilNumber Hierarchy - SubDistrictName':'Boro', 
-           'Current Month':month,
-           'CouncilNumber Hierarchy - Unit':'Unit'}
+    full = full.rename(re_name,axis=1)
+    full = full[['Boro','District','Unit','Order',month]]
+    full= full[~full['Boro'].isna()]
+    full['Boro'] = full['Boro'].apply(lambda x: x.split(' (')[0].split(' 6')[0])
+    full['Unique'] = full['Boro']+full['District']+full['Unit']
 
-full = full.rename(re_name,axis=1)
-full = full[['Boro','District','Unit','Order',month]]
-full= full[~full['Boro'].isna()]
-full['Boro'] = full['Boro'].apply(lambda x: x.split(' (')[0].split(' 6')[0])
-full['Unique'] = full['Boro']+full['District']+full['Unit']
-
-if len(df) == 0:
-    df = pd.concat([df,full],axis = 0)
-    df_net = pd.concat([df_net,df[['Unique','Boro','District','Unit','Order']]],axis = 0)
-    df_net = df_net.set_index('Unique')
-else:
-    df[month] = df['Unique'].map(full.set_index('Unique')[month])
-    df = df.fillna(0.0)
-
-newbies = pd.read_excel(uploaded_file_ny,skiprows=2)
-newbies = newbies.rename(re_name,axis=1)
-newbies = newbies[['Boro','District','Unit','RegStatusxMonth','Month Year']]
-newbies= newbies[~newbies['Boro'].isna()]
-newbies['Boro'] = newbies['Boro'].apply(lambda x: x.split(' (')[0].split(' 6')[0])
-newbies['Unique'] = newbies['Boro']+newbies['District']+newbies['Unit']
-
-df_ny['Unique'] = df['Unique']
-df_ny['Boro'] = df['Boro']
-df_ny['District'] = df['District']
-df_ny['Order'] = df['Order']
-df_ny['Unit']= df['Unit']
-df_ny = df_ny.set_index('Unique').fillna(0.0)
-
-for col in months:
-    frame = newbies[newbies['Month Year']==col]
-    for _,row in frame.iterrows():
-        df_ny.loc[row.Unique,col] = row['RegStatusxMonth']
-        
-
-#curr_mon=2
-for _,row in df.iterrows():
-    curr = mon_dict[curr_mon]
-    if curr_mon != 1:
-        past = mon_dict[curr_mon-1]
+    if len(df) == 0:
+        df = pd.concat([df,full],axis = 0)
+        df_net = pd.concat([df_net,df[['Unique','Boro','District','Unit','Order']]],axis = 0)
+        df_net = df_net.set_index('Unique')
     else:
-        past = curr
-    net = row[curr] - row[past]
-    r=row.Unique
-    c=curr
-    df_net.loc[r,c] = net
+        df[month] = df['Unique'].map(full.set_index('Unique')[month])
+        df = df.fillna(0.0)
 
-df_ny['Total New Youth'] = df_ny[months].sum(axis=1)
+    newbies = pd.read_excel(uploaded_file_ny,skiprows=2)
+    newbies = newbies.rename(re_name,axis=1)
+    newbies = newbies[['Boro','District','Unit','RegStatusxMonth','Month Year']]
+    newbies= newbies[~newbies['Boro'].isna()]
+    newbies['Boro'] = newbies['Boro'].apply(lambda x: x.split(' (')[0].split(' 6')[0])
+    newbies['Unique'] = newbies['Boro']+newbies['District']+newbies['Unit']
 
-df_ny['Net Change from January'] = df_net[months].sum(axis=1)
-df_ny['Current Size'] = df.set_index('Unique')[month]
+    df_ny['Unique'] = df['Unique']
+    df_ny['Boro'] = df['Boro']
+    df_ny['District'] = df['District']
+    df_ny['Order'] = df['Order']
+    df_ny['Unit']= df['Unit']
+    df_ny = df_ny.set_index('Unique').fillna(0.0)
 
-display = df_ny[['District', 'Unit','Order','Total New Youth','Net Change from January','Current Size']].reset_index().drop('Unique',axis=1)
+    for col in months:
+        frame = newbies[newbies['Month Year']==col]
+        for _,row in frame.iterrows():
+            df_ny.loc[row.Unique,col] = row['RegStatusxMonth']
+            
 
-df.to_csv('Monthly Membership by unit.csv')
-df_net.to_csv('Net change by month.csv')
-df_ny.to_csv('New Youth.csv')
+    #curr_mon=2
+    for _,row in df.iterrows():
+        curr = mon_dict[curr_mon]
+        if curr_mon != 1:
+            past = mon_dict[curr_mon-1]
+        else:
+            past = curr
+        net = row[curr] - row[past]
+        r=row.Unique
+        c=curr
+        df_net.loc[r,c] = net
+
+    df_ny['Total New Youth'] = df_ny[months].sum(axis=1)
+
+    df_ny['Net Change from January'] = df_net[months].sum(axis=1)
+    df_ny['Current Size'] = df.set_index('Unique')[month]
+
+    display = df_ny[['District', 'Unit','Order','Total New Youth','Net Change from January','Current Size']].reset_index().drop('Unique',axis=1)
+
+    df.to_csv('Monthly Membership by unit.csv')
+    df_net.to_csv('Net change by month.csv')
+    df_ny.to_csv('New Youth.csv')
