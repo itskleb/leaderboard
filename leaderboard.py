717277702300a24b3805dadfddeff0b22ff373af
itskleb <itskleb@gmail.com>
2026-03-26 11:53:54 -0400

    ]

--- leaderboard.py
+++ leaderboard.py
@@ -117,10 +117,10 @@ with tab1:
     col1, col2, col3 = st.columns(3)
     with col1:
         st.write(display.sort_values(col_sort,ascending=False)['Unit'][0])
-        st.metrics(label = '1st Place', value = display.sort_values(col_sort,ascending=False)[col_sort].loc[0])
+        st.metric(label = '1st Place', value = display.sort_values(col_sort,ascending=False)[col_sort].loc[0])
     with col2:
         st.write(display.sort_values(col_sort,ascending=False)['Unit'][1])
-        st.metrics(label = '1st Place', value = display.sort_values(col_sort,ascending=False)[col_sort].loc[1])
+        st.metric(label = '1st Place', value = display.sort_values(col_sort,ascending=False)[col_sort].loc[1])
     with col3:
         st.write(display.sort_values(col_sort,ascending=False)['Unit'][2])
-        st.metrics(label = '1st Place', value = display.sort_values(col_sort,ascending=False)[col_sort].loc[2])
+        st.metric(label = '1st Place', value = display.sort_values(col_sort,ascending=False)[col_sort].loc[2])
